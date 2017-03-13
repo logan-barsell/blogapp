e104c6a7ede47660117c12c63a62ab7f9cb736c0
Logan Barsell <loganjbars@gmail.com>
2017-03-13 20:35:54 +0100

    Added background pic to home, messages section

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,26 +1,11 @@
 $(document).ready(f => {
+
 	$('.collapsible').collapsible()
-	var counting = 0
-	$('ul.collapsible').click(f=> {
-		counting ++
-		if (counting%2){
-			$('.collapsible-header').html('Hide Comments')
-		}else {
-			$('.collapsible-header').html('Show Comments')
-		}
-	})
+
+ //    if ($('li').hasClass('active')) {
+	// 	console.log('Yes, it worked!')
+	// 	$('.collapsible-header').html('Hide Comments')
+	// } else {
+	// 	$('.collapsible-header').html('Show Comments')
+	// }
 })	
-// var counting = 0
-//  	$('ul.collapsible').click(f => {
-// 		counting++
-//  		if (counting%2){
-//  			$(this).html('Hide Comments')
-//  		}else{
-//  			$(this).html('Show Comments')
-// 		}
-// 	})    if ($('ul.collapsible > li').hasClass('active')) {
-// 		console.log('Yes, it worked!')
-// 		$('.collapsible-header').html('Hide Comments')
-//     } else {
-//     	$('.collapsible-header').html('Show Comment')
-//     }
