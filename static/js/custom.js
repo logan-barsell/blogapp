ced2a7e9443d4ff3c7e42f2269918b49cd80ef1e
Logan Barsell <loganjbars@gmail.com>
2017-03-14 13:30:05 +0100

    Added fade in/out function to stone nav, background pic to profile

--- static/js/custom.js
+++ static/js/custom.js
@@ -4,15 +4,15 @@ $(document).ready(f => {
 
 
 	$(window).bind('scroll', f => {
-	   	var navHeight = $( window ).height() + 353	 
+	   	var navHeight = $( window ).height() + 277	 
 	   		if ($(window).scrollTop() > navHeight) {
 				$('.stone').addClass('fixed')
-				$('.img').addClass('afterscroll')
+				$('.palms').addClass('afterscroll')
 				$('a.fading').fadeIn(1500)
 			}
 			else {
 				$('.stone').removeClass('fixed')
-				$('.img').removeClass('afterscroll')
+				$('.palms').removeClass('afterscroll')
 				$('a.fading').fadeOut(1500)
 			}
 		})
